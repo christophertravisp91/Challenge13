87135ad497b3f4a0fa4fca1138a2f3af13d3bcc6
TomFoolery <christophertravisp91@gmail.com>
2022-03-12 14:54:21 -0500

    sql

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -4,8 +4,10 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize(process.env.DB_NAME = 'ecommerce_db', process.env.DB_USER = 'root', process.env.DB_PW = 'ChPiAr121191$', {
       host: 'localhost',
+      username:'root',
+      password: 'ChPiAr121191$',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
